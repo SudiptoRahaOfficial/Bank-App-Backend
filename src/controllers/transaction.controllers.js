861bef75401bd732493af6c8144b7e450054afc1
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-20 22:22:05 +0600

    added core transaction functionality at transaction create api

--- src/controllers/transaction.controllers.js
+++ src/controllers/transaction.controllers.js
@@ -4,6 +4,7 @@
  */
 
 // importing dependencis
+const { startSession } = require('mongoose')
 const transactionModel = require('../models/transaction.model')
 const ledgerModel = require('../models/ledger.model')
 const accountModel = require('../models/account.model')
@@ -80,8 +81,60 @@ async function createTransaction(req, res) {
 			})
 		}
 
-        // validating fromUserAccount has sufficient balance
-        const fromUserAccountBalance = await fromUserAccount.get
+		// validating fromUserAccount has sufficient balance
+		const fromUserAccountBalance = await fromUserAccount.getAccountBalance()
+		if (fromUserAccountBalance < amount) {
+			return res.status(400).json({
+				message: 'Insufficient balance to make transaction',
+				status: 'failed',
+			})
+		}
+
+		// starting session for transaction
+		const transactionSession = await startSession()
+		transactionSession.startTransaction()
+
+		// creating transaction - status: pending
+		const transaction = await transactionModel.create(
+			{
+				fromAccount,
+				toAccount,
+				amount,
+				idempotencyKey,
+				status: 'PENDING',
+			},
+			{ transactionSession },
+		)
+
+		// creating debit-ledger-entry for fromAccount
+		const debitLedgerEntry = await ledgerModel.create(
+			{
+				transaction: transaction._id,
+				fromAccount,
+				amount,
+				type: 'DEBIT',
+			},
+			{ transactionSession },
+		)
+
+		// creating credit-ledger-entry for toAccount
+		const creditLedgerEntry = await ledgerModel.create(
+			{
+				transaction: transaction._id,
+				toAccount,
+				amount,
+				type: 'CREDIT',
+			},
+			{ transactionSession },
+		)
+
+		// after successful debit & credit updating transaction-status
+		transaction.status = 'COMPLETED'
+		await transaction.save({ transactionSession })
+
+        // ending transaction session
+        await transactionSession.commitTransaction()
+        transactionSession.endSession()
 	} catch (error) {
 		// logging on unexpected server error
 		console.error('Transaction failed', {
