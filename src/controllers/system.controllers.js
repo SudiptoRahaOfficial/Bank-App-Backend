ab3be6d468f79ea1b7a7294d6f31db6dd52f7c85
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-21 14:03:25 +0600

    added basic validation to initialFundController

--- src/controllers/system.controllers.js
+++ src/controllers/system.controllers.js
@@ -10,7 +10,31 @@
     - POST API - "/api/system/initial-fund"
  */
 async function initialFundController(req, res) {
-	
+	// extracting all data sent by client
+	const { toAccount, amount, idempotencyKey } = req.body
+
+	// validating required fields
+	if (!toAccount || !amount || !idempotencyKey) {
+		return res.status(400).json({
+			message: 'ToAccount, amount & idempotencyKey are required',
+			status: 'failed',
+		})
+	}
+
+	// validating transaction amount
+	if (
+		typeof amount !== 'number' ||
+		!Number.isFinite(amount) ||
+		amount <= 999
+	) {
+		return res.status(400).json({
+			message: 'Invalid amount! Minimum deposit amount is 1000',
+			status: 'failed',
+		})
+	}
+
+	try {
+	} catch (error) {}
 }
 
 // exporting controllers
